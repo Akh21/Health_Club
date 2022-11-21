125250eb1f787c903485fe7a27fa27469016ab8d
Akash Singh <akash.singh2@happiestminds.com>
2022-11-21 16:04:40 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactusComponent } from './contactus/contactus.component';
+import { HomeComponent } from './home/home.component';
+import { PlaceappointmentComponent } from './placeappointment/placeappointment.component';
+import { ViewappointmentComponent } from './viewappointment/viewappointment.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"home",
+    component:HomeComponent
+  },
+  {
+    path:"viewappointment",
+    component:ViewappointmentComponent
+  },
+  {
+    path:"placeappointment",
+    component:PlaceappointmentComponent
+  },
+  {
+    path:"contactus",
+    component:ContactusComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ViewappointmentComponent } from './viewappointment/viewappointment.component';
+import { PlaceappointmentComponent } from './placeappointment/placeappointment.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ViewappointmentComponent,
+    PlaceappointmentComponent,
+    ContactusComponent
   ],
   imports: [
     BrowserModule,
